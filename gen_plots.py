512017a171a837f612c2c955500b1c7fcec4b616
Varun Nambiar <nambvarun@gmail.com>
2019-10-08 17:22:55 -0700

    fix: added documentation

--- gen_plots.py
+++ gen_plots.py
@@ -6,11 +6,22 @@ import os
 
 
 def get_params(p: argparse.ArgumentParser) -> argparse.Namespace:
+    """
+    Gets the parameters passed when the script is called.
+    :param p: the argument parser
+    :return: a argparse.Namespace obj with the parsed arguments
+    """
     p.add_argument('-f', '--folder', help='Folder relative to script location with CSVs to parse.', default='outputs')
     return p.parse_args()
 
 
 def parse_and_gen_plots(rel_folder_path: str) -> None:
+    """
+    Parses the CSVs in a given folder and outputs a plot for each CSV.
+
+    :param rel_folder_path: folder path with respect to the script location
+    :return: None
+    """
     sns.set()
     sns.set_context("notebook")
 
